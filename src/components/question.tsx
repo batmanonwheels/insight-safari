0e2964dc2bf709fda010a6fd888aa12a47d3cb8c
Devin Garcia <devingarcia5423@gmail.com>
2026-05-08 16:30:26 -0400

    adjust font sizes

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -10,12 +10,12 @@ export const Question = ({ text }: QuestionProps) => {
 
   return (
     <section
-      className={`relative min-h-screen min-w-screen flex items-center justify-center bg-[url(/backgrounds/ferns-1.webp)] bg-green-800 bg-cover snap-center text-white text-9xl font-bold `}
+      className={`relative min-h-screen min-w-screen flex items-center justify-center bg-[url(/backgrounds/ferns-1.webp)] bg-green-800 bg-cover snap-center text-white text-7xl font-bold `}
     >
       <div className="w-3/4 flex flex-col items-center justify-center bg-green-800 text-white p-15 rounded-2xl outline-10 outline-offset-10">
         <h2 className="m-auto">{text}</h2>
       </div>
-      <div className="absolute left-0 bottom-0 w-screen flex items-center justify-around p-4 text-6xl ">
+      <div className="absolute left-0 bottom-0 w-screen flex items-center justify-around p-4">
         <Image
           height={1000}
           width={1000}
@@ -23,7 +23,7 @@ export const Question = ({ text }: QuestionProps) => {
           alt={"Arrow pointing to the left"}
           className="w-3/4"
         />
-        <p className="w-1/4 ">SWIPE</p>
+        <p className="w-1/4 text-5xl">SWIPE</p>
       </div>
     </section>
   );
