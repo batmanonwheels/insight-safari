7c8868e037e71a7d78330d9f407c3ee38f499bd7
Devin Garcia <devingarcia5423@gmail.com>
2026-05-19 11:48:16 -0400

    decrease font size

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -12,7 +12,7 @@ export const Question = ({ text, orientation, first }: QuestionProps) => {
       className={`@container relative w-3/4 h-auto min-w-0 min-h-0 flex flex-col font-bold items-center justify-center bg-[#0C7158] text-white p-12 rounded-4xl outline-10`}
       id={first ? "first-speech-bubble" : undefined}
     >
-      <h2 className={`m-auto w-full h-full text-[18cqi]`}>{text}</h2>
+      <h2 className={`m-auto w-full h-full text-[15cqi]`}>{text}</h2>
       <BubblePointer orientation={orientation} />
     </div>
   );
