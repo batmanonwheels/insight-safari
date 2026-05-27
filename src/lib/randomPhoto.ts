69b7c0f62aff9269efe9299840056359a20481d3
Devin Garcia <devingarcia5423@gmail.com>
2026-05-27 06:20:35 -0400

    update photo array with new images

--- src/lib/randomPhoto.ts
+++ src/lib/randomPhoto.ts
@@ -1,7 +1,9 @@
 export const randomPhoto = (): string => {
 	const photos = [
 		'/backgrounds/ferns-1.webp',
-		// '/backgrounds/ferns-2.webp'
+		'/backgrounds/ferns-2.webp',
+		'/backgrounds/ferns-3.webp',
+		'/backgrounds/ferns-4.webp',
 	];
 	return photos[Math.floor(Math.random() * photos.length)];
 };
