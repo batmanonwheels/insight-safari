670468cc9c186a71f1e7fe98a709ffe5ccd9b91c
Devin Garcia <devingarcia5423@gmail.com>
2026-05-30 15:07:59 -0400

    update next config

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+	/* config options here */
+	reactCompiler: true,
+	allowedDevOrigins: ['192.168.8.106'],
+};
+
+export default nextConfig;
