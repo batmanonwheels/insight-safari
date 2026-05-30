e7c3421c847c4e6013dada867c14ea8bfe9dc70d
Devin Garcia <devingarcia5423@gmail.com>
2026-05-30 13:59:09 -0400

    update sw path

--- src/app/serwist/[path]/route.ts
+++ src/app/serwist/[path]/route.ts
@@ -16,24 +16,19 @@ const revision =
 	spawnSync('git', ['rev-parse', 'HEAD'], { encoding: 'utf-8' }).stdout ??
 	crypto.randomUUID();
 
-export const {
-	dynamic = 'force-dynamic',
-	dynamicParams,
-	revalidate,
-	generateStaticParams,
-	GET,
-} = createSerwistRoute({
-	additionalPrecacheEntries: [
-		{ url: '/~offline', revision },
-		{ url: '/', revision },
-		{ url: '/communications', revision },
-		{ url: '/equity-education', revision },
-		{ url: '/financial-wellness', revision },
-		{ url: '/platform-support', revision },
-		{ url: '/tax-insights', revision },
-	],
-	swSrc: 'app/sw.ts',
-	// nextConfig,
-	// If set to `false`, Serwist will attempt to use `esbuild-wasm`.
-	useNativeEsbuild: true,
-});
+export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
+	createSerwistRoute({
+		additionalPrecacheEntries: [
+			{ url: '/~offline', revision },
+			{ url: '/', revision },
+			{ url: '/communications', revision },
+			{ url: '/equity-education', revision },
+			{ url: '/financial-wellness', revision },
+			{ url: '/platform-support', revision },
+			{ url: '/tax-insights', revision },
+		],
+		swSrc: 'src/app/sw.ts',
+		// nextConfig,
+		// If set to `false`, Serwist will attempt to use `esbuild-wasm`.
+		useNativeEsbuild: true,
+	});
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,23 +6,25 @@ import { handleFullscreen } from "./fullscreen";
 export const Nav = () => {
   return (
     <nav className="flex flex-wrap justify-center text-3xl gap-10 *:px-15 *:py-10 *:bg-[#0C7158] *:border-10 *:rounded-2xl">
-      <Link href="/equity-education" replace>
+      <Link href="/equity-education" replace onClick={() => handleFullscreen()}>
         EQUITY EDUCATION
       </Link>
-      <Link href="/communications" replace>
+      <Link href="/communications" replace onClick={() => handleFullscreen()}>
         COMMUNICATIONS
       </Link>
-      <Link href="/financial-wellness" replace>
+      <Link
+        href="/financial-wellness"
+        replace
+        onClick={() => handleFullscreen()}
+      >
         FINANCIAL WELLNESS
       </Link>
-      <Link href="/platform-support" replace>
+      <Link href="/platform-support" replace onClick={() => handleFullscreen()}>
         PLATFORM SUPPORT
       </Link>
-      <Link href="/tax-insights" replace>
+      <Link href="/tax-insights" replace onClick={() => handleFullscreen()}>
         TAX INSIGHTS
       </Link>
     </nav>
   );
 };
-
-//  onClick={() => handleFullscreen()}
